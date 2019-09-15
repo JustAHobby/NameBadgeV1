616b54e971911e28c1f50f700c0200d9719b61b4
Matthew Byington <matt@MacBook-Pro-2.local>
2019-09-15 14:16:23 -0700

    added button support to cycle between names

--- NameBadgeV1.ino
+++ NameBadgeV1.ino
@@ -7,6 +7,8 @@ const int  buttonPin = 1;    // the pin that the pushbutton is attached to
 
 int buttonState = 0;         // current state of the button
 int lastButtonState = 0;     // previous state of the button
+int currentName = 1;    // loop to call what name to display
+
 
 void setup() {
   Serial.begin(9600);
@@ -33,7 +35,6 @@ void setup() {
   alpha4.clear();
   alpha4.writeDisplay();
   writeName();
-  Serial.println("ready");
 }
 
 void loop() {
@@ -42,17 +43,28 @@ void loop() {
 
   if (buttonState != lastButtonState) {
     if (buttonState == HIGH) {
-      // clean display to show button was pressed
-      alpha4.clear();
-      alpha4.writeDisplay();
-    } else {
-      writeName();
+      // display the next name and iterate/loop the name;
+      currentName = currentName + 1;
+      if(currentName > 3) { //we have 3 names so if we hit 4 loop back
+        currentName = 1;
+      }
+      nextName();
     }
     // Delay a little bit to avoid bouncing
     delay(50);
   }
   // save the current state as the last state, for next time through the loop
   lastButtonState = buttonState;
+}
+
+void nextName() {
+  if(currentName == 1) {
+    writeName();
+  } else if(currentName == 2) {
+    writeDept();
+  } else if(currentName == 3) {
+    writeAltName();
+  }
 }
 
 void writeName() {
@@ -65,6 +77,38 @@ void writeName() {
   alpha4.writeDigitAscii(1, 'A');
   alpha4.writeDigitAscii(2, 'T');
   alpha4.writeDigitAscii(3, 'T');
+  
+  // write it out
+  alpha4.writeDisplay();
+  delay(200);
+}
+
+void writeDept() {
+  // pre clear display
+  alpha4.clear();
+  alpha4.writeDisplay();
+  
+  // setup characters for name
+  alpha4.writeDigitAscii(0, 'D');
+  alpha4.writeDigitAscii(1, 'T');
+  alpha4.writeDigitAscii(2, 'C');
+  alpha4.writeDigitAscii(3, 'I');
+  
+  // write it out
+  alpha4.writeDisplay();
+  delay(200);
+}
+
+void writeAltName() {
+  // pre clear display
+  alpha4.clear();
+  alpha4.writeDisplay();
+  
+  // setup characters for name
+  alpha4.writeDigitAscii(0, 'Z');
+  alpha4.writeDigitAscii(1, 'U');
+  alpha4.writeDigitAscii(2, 'R');
+  alpha4.writeDigitAscii(3, 'G');
   
   // write it out
   alpha4.writeDisplay();
